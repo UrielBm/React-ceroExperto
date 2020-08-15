24a1ad67e8c06ba5820c6309c9e3a7d6f94c7c43
Uriel Benítez Medina <urielbenitez96@hotmail.com>
2020-08-15 14:29:56 -0500

    Subiendo primera App y modificaciones en index html

---
+++ src/PrimeraApp.js
@@ -0,0 +1,6 @@
+import React from 'react';
+const PrimeraApp=() => {
+
+    return <h1>Holi crayoli</h1>
+}
+export default PrimeraApp;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PrimeraApp from './PrimeraApp';
+import './index.css'
+
+//const saludo =<h1>Que onda la banda</h1>
+const divRoot = document.querySelector('#Mi_App');
+
+ReactDOM.render(<PrimeraApp />,divRoot);
